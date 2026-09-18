61056d147749ea30276e0c8ed823181e1219107a
TakWolf <takwolf@foxmail.com>
2026-09-18 15:48:48 +0800

    调整变量名

--- tools/services/publish_service.py
+++ tools/services/publish_service.py
@@ -15,13 +15,14 @@ def make_release_zips(dump_logs: Mapping[LanguageFlavor, list[int]], font_format
 
     for language_flavor in options.LANGUAGE_FLAVORS:
         for font_format in font_formats:
-            file_path = path_define.RELEASES_DIR.joinpath(f'zfull-pixel-font-{language_flavor}-{font_format}-v{configs.VERSION}.zip')
-            with ZipFile(file_path, 'w') as file:
+            zip_file_path = path_define.RELEASES_DIR.joinpath(f'zfull-pixel-font-{language_flavor}-{font_format}-v{configs.VERSION}.zip')
+            with ZipFile(zip_file_path, 'w') as file:
                 file.write(path_define.PROJECT_ROOT_DIR.joinpath('LICENSE-FONT.md'), 'README.md')
+
                 for font_size in dump_logs[language_flavor]:
-                    file_path = path_define.OUTPUTS_DIR.joinpath(f'Zfull-{language_flavor.upper()}-{font_size}px.{font_format}')
-                    file.write(file_path, file_path.name)
-            logger.info("Make release zip: '{}'", file_path)
+                    font_file_path = path_define.OUTPUTS_DIR.joinpath(f'Zfull-{language_flavor.upper()}-{font_size}px.{font_format}')
+                    file.write(font_file_path, font_file_path.name)
+            logger.info("Make release zip: '{}'", zip_file_path)
 
 
 def update_www(dump_logs: Mapping[LanguageFlavor, list[int]]) -> None:
@@ -32,6 +33,7 @@ def update_www(dump_logs: Mapping[LanguageFlavor, list[int]]) -> None:
     for path_from in path_define.OUTPUTS_DIR.iterdir():
         if not path_from.name.endswith('.otf.woff2'):
             continue
+
         path_to = path_from.copy_into(path_define.WWW_FONTS_DIR)
         logger.info("Copy file: '{}' -> '{}'", path_from, path_to)
 
